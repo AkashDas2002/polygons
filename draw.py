77311dea85aadb215eee86eac4475a9fc448fd0a
Akash Das <akashdas.ny@gmail.com>
2020-03-29 15:37:32 -0400

    changed a little

--- draw.py
+++ draw.py
@@ -38,11 +38,11 @@ def add_box( polygons, x, y, z, width, height, depth ):
     y1 = y - height
     z1 = z - depth
 
-    add_polygon(polygons, x, y, z, x, y1, z, x1, y, z)
-    add_polygon(polygons, x, y1, z, x1, y1, z, x1, y, z)
+    add_polygon(polygons, x, y, z, x, y1, z, x1, y1, z)
+    add_polygon(polygons, x1, y, z, x, y, z, x1, y1, z)
 
-    add_polygon(polygons, x1, y, z, x1, y1, z, x1, y, z1)
-    add_polygon(polygons, x1, y1, z, x1, y1, z1, x1, y, z1)
+    add_polygon(polygons, x1, y, z, x1, y1, z, x1, y1, z1)
+    add_polygon(polygons, x1, y1, z1, x1, y, z1, x1, y, z)
 
     add_polygon(polygons, x1, y1, z1, x, y1, z1, x1, y, z1)
     add_polygon(polygons, x, y1, z1, x, y, z1, x1, y, z1)
